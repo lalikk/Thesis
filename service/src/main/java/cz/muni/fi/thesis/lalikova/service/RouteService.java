17700c356252dee1594aac66a582e923f5a87d87
Kristina Lalikova <xlalikov@fi.muni.cz>
2021-10-23 13:21:23 +0200

    Created service layer, Service interfaces as a delegation of dao calls

---
+++ service/src/main/java/cz/muni/fi/thesis/lalikova/service/RouteService.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.thesis.lalikova.service;
+
+import cz.muni.fi.thesis.lalikova.entity.Route;
+import java.util.List;
+
+public interface RouteService {
+    void create(Route route);
+
+    Route findById(Long id);
+
+    List<Route> findAll();
+
+    boolean isOrdered(Route route);
+
+    Long getStaringPointId(Route route);
+
+    void update(Route route);
+
+    void remove(Route route);
+}
