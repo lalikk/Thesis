b1c561921a1ed71fce38be409615eb7d4f44fcf5
Kristina Lalikova <xlalikov@fi.muni.cz>
2021-11-08 21:38:21 +0100

    User added to service layer

---
+++ service/src/main/java/cz/muni/fi/thesis/lalikova/service/AdvancedUserService.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.thesis.lalikova.service;
+
+import cz.muni.fi.thesis.lalikova.entity.AdvancedUser;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface AdvancedUserService {
+
+    boolean authenticate(AdvancedUser user, String password);
+
+    AdvancedUser findUserById(Long id);
+
+    List<AdvancedUser> findAll();
+
+    AdvancedUser findByLogin(String login);
+}
