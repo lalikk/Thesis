ee119bfc42f068710ea812d7fc138009a5be50c3
Kristina Lalikova <xlalikov@fi.muni.cz>
2021-11-13 16:19:46 +0100

    Rest helper classes created

---
+++ rest_api/src/main/java/cz/muni/fi/thesis/lalikova/rest_api/ApiUri.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.thesis.lalikova.rest_api;
+
+public enum ApiUri {
+
+    ROOT_URI ("/rest"),
+
+    ROOT_URI_USER ("/users/{id}"),
+    ROOT_URI_USERS("/users"),
+
+    ROOT_URI_COORDINATE ("/coordinates/{id}"),
+    ROOT_URI_COORDINATES("/coordinates"),
+
+    ROOT_URI_PHOTO ("/photos/{id}"),
+    ROOT_URI_PHOTOS("/photos"),
+
+    ROOT_URI_ROUTE ("/routes/{id}"),
+    ROOT_URI_ROUTES("/routes"),
+
+    ROOT_URI_PHOTO_TAG ("/point_tags/{id}"),
+    ROOT_URI_PHOTO_TAGS("/point_tags"),
+
+    ROOT_URI_POINT ("/points/{id}"),
+    ROOT_URI_POINTS("/points");
+
+    private final String uri;
+    ApiUri(String uri) {
+        this.uri = uri;
+    }
+
+    private String getUri() {
+        return uri;
+    }
+
+}
---
+++ rest_api/src/main/java/cz/muni/fi/thesis/lalikova/rest_api/ResponseStatus.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.thesis.lalikova.rest_api;
+
+public enum ResponseStatus {
+    OK,
+    ERROR,
+    NOT_FOUND
+}
