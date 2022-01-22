aaf4635d704e7a64e3b0598179c61a99f4e15a64
lalikk <xlalikov@fi.muni.cz>
2022-01-22 16:57:02 +0100

    pom.xml fixes, change in photo entity

--- persistence/src/main/java/cz/muni/fi/thesis/lalikova/entity/Photo.java
+++ persistence/src/main/java/cz/muni/fi/thesis/lalikova/entity/Photo.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
@@ -21,7 +20,6 @@ public class Photo {
     @Column(nullable = false)
     private String description;
 
-    @Lob
     @Column(nullable=false)
     private @NotNull String image;
 
