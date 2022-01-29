ac0dca0643ce17c75dc0ac0707c0ec17fd52aad0
lalikk <xlalikov@fi.muni.cz>
2022-01-29 12:18:51 +0100

    user movement on route + displaying passed route

--- frontend/route_navigation.js
+++ frontend/route_navigation.js
@@ -28,7 +28,7 @@ initMap();
 
 let routePointsPromise = new Promise((success, error) => {
   $.getJSON('http://localhost:8080/rest/points', (data) => {
-    console.log(data);  
+    //console.log(data);  
     let routePoints = [];
     var indexInRoute = 1;
     for (let point of data) {
@@ -56,12 +56,15 @@ let routePromise = new Promise(async (success, error) => {
   if(recompute === "false") {
     let g = window.localStorage.getItem('geometry');
     let geometry = JSON.parse(g);
-    let coords = [];
-    for (let c of geometry.coords) {
-      coords.push(SMap.Coords.fromWGS84(c.x, c.y));
+    let geometries = [];
+    for (let g of geometry) {
+      let coords = [];
+      for (let c of g.coords) {
+        coords.push(SMap.Coords.fromWGS84(c.x, c.y));
+      }
+      geometries.push(new SMap.Geometry(g.type, null, coords, g.options));
     }
-    let newGeometry = new SMap.Geometry(geometry.type, null, coords, geometry.options);
-    success(newGeometry);
+    success(geometries);
   } else {
     var coords = [];
     // TODO: Handle case when location is not available (promis throws exception).
@@ -71,25 +74,25 @@ let routePromise = new Promise(async (success, error) => {
     for (let routePoint of userRoute) {
       coords.push(routePoint.SMapCoords);
     }
-    console.log(coords);
     // Add error handling
     SMap.Route.route(coords, { geometry: true, itinerary:true }).then((route) => {
-      var coords = route.getResults().geometry;
-      console.log(route.getResults());
+      var routeResults = route.getResults();
+      var coords = routeResults.geometry;
       var g = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
       // slice geometry
-      storeGeometry(g);
-      success(g);
+      let geometryPerSegments = sliceGeometry(g, routeResults.points);
+      storeGeometry(geometryPerSegments);
+      // which segment of route user is on, always start at beginning as user location = 1st point
+      Cookies.set('userProgress', '0');
+      success(geometryPerSegments);
     });
   }
 });
 
-displayRoute(await routePromise, null);
-
 displayPointMarkers(await routePointsPromise);
+displayRoute(await routePromise, null);
 updateUserMarker(await locationPromise);
 displayRoute(await routePromise, await locationPromise);
-
 
 navigator.geolocation.watchPosition(function (position) {
   position["SMapCoords"] = SMap.Coords.fromWGS84(position.coords.longitude, position.coords.latitude);
@@ -111,9 +114,63 @@ function displayPointMarkers(points) {
 }
 
 function displayRoute(routeGeometry, userLocation) {
-  layerGeometry.addGeometry(routeGeometry);
+  if (userLocation != null) {
+    changeRouteProgress(routeGeometry, userLocation);   
+  }
+  for (let g of routeGeometry) {
+    layerGeometry.addGeometry(g);
+  }
   layerGeometry.redraw();
 }
+
+function changeRouteProgress(routeGeometry, userLocation) {
+  let segmentProgress = Cookies.get('userProgress');
+  for (let i = 0; i < segmentProgress; ++i) {
+    routeGeometry[i]._options.color = 'gray';
+  }
+  let closestPoint = findUserOnRoute(routeGeometry[segmentProgress].getCoords(), userLocation);
+  let visitedPart = routeGeometry[segmentProgress]._coords.splice(0, closestPoint+1);
+  let visitedGeometry = new SMap.Geometry(routeGeometry[segmentProgress].getType(), null, visitedPart, routeGeometry[segmentProgress].getOptions());
+  visitedGeometry._options.color='gray';
+  routeGeometry.splice(segmentProgress, 0, visitedGeometry);
+}
+
+function findUserOnRoute(route, user) {
+  if (route.length == 0) {
+    return 0;
+  }
+  let firstPointDistance = calcDistance(user.SMapCoords, route[0]);
+  var smallestDistance = {index:0, distance:firstPointDistance}
+  for (let i =0; i < route.length; ++i) {
+    let distance = calcDistance(user.SMapCoords, route[i]);
+    if (distance < smallestDistance.distance) {
+      smallestDistance.index = i;
+      smallestDistance.distance = distance;
+    }
+  }
+  return smallestDistance.index;
+}
+
+function calcDistance(coords1, coords2) {
+	if ((coords1.x == coords2.x) && (coords1.y == coords2.y)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * coords1.y/180;
+		var radlat2 = Math.PI * coords2.y/180;
+		var theta = coords1.x-coords2.x;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60;
+		return dist;
+	}
+}
+
 
 function updateUserMarker(userLocation) {
   let markers = layerMarkers.getMarkers();
@@ -183,8 +240,38 @@ function reactToMarkerClick() {
 }
 
 function storeGeometry(g) {
-  let geometry = {type: g.getType(), coords: g.getCoords(), options:g.getOptions()};
+  let geometry = [];
+  for (let gItem of g) {
+     geometry.push({type: gItem.getType(), coords: gItem.getCoords(), options:gItem.getOptions()});
+  }
   let jsonGeometry = JSON.stringify(geometry);
   window.localStorage.setItem('geometry', jsonGeometry);
   Cookies.set('navigationRecompute', 'false');
+}
+
+function sliceGeometry(geometry, points) {
+  let geometrySegments = [];
+  var partingIndex = 0;
+  // skip leading zeroes
+  while (points[partingIndex].index == 0) {
+    ++partingIndex;
+  }
+  for (; partingIndex < points.length; ++partingIndex) {
+    if(points[partingIndex].index == 0) {
+      geometrySegments.push(geometry._coords.splice(0, points[partingIndex-1].index+1));
+      while (points[partingIndex].index == 0) {
+        ++partingIndex;
+      }
+    }
+  }
+  geometrySegments.push(geometry._coords);
+  return createGeometries(geometrySegments, geometry);
+}
+
+function createGeometries(geometrySegments, geometry) {
+  let routeGeometry = [];
+  for (let segment of geometrySegments) {
+    routeGeometry.push(new SMap.Geometry(geometry.getType(), null, segment, geometry.getOptions()));
+  }
+  return routeGeometry;
 }
